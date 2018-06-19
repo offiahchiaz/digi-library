a645d1fd5a6a376e93c34e6c63563099b8191227
offiahchiaz <offiahchiaz@gmail.com>
2018-06-19 01:29:55 +0100

    first commits

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/cool', function(req, res, next) {
+  res.send("God's word is a compedium of all the possibilities that are resident in God.");
+});
+
+module.exports = router;
